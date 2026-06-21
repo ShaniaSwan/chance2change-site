d4c98686bfac713967783a477d86352fbaa8a5d2
ShaniaSwan <shaniaswan0012@gmail.com>
2026-06-21 15:48:36 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function moveSlide(carouselId, direction) {
-  const carousel = document.getElementById(carouselId);
-  const scrollAmount = 340;
-  carousel.scrollBy({
-    left: direction * scrollAmount,
-    behavior: "smooth"
-  });
-}
